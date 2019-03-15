a69024af6e1a038ff9f1ef34072eb9a7dbf969e1
Erin King <eohone@gmail.com>
2019-03-14 20:04:14 -0600

    adds passing test for unique_hometown_list

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -23,11 +23,13 @@ RSpec.describe Comedian, type: :model do
       end
     end
 
-    describe ".unique_hometown_list"
-      it "returns a unique list of hometowns"
+    describe ".unique_hometown_list" do
+      it "returns a unique list of hometowns" do 
         Comedian.create(name: "Iliza Shlesinger", age: 26, hometown: "Manhattan", profile_pic: "http://avalonuk.com/wp-content/uploads/2015/03/iliza-website.jpg")
         Comedian.create(name: "Erin King", age: 34, hometown: "Fountain", profile_pic: "http://avalonuk.com/wp-content/uploads/2015/03/iliza-website.jpg")
 
         expect(Comedian.unique_hometown_list.count).to eq(2)
       end
     end
+  end
+end
